c7eea929cf4c2fdbc07bc5db247cd4346eb8a7bf
shunJS <75880904+shunJS@users.noreply.github.com>
2021-02-16 13:02:46 +0900

    clone airbnb1

--- src/Banner.js
+++ src/Banner.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
+import Search from "./Search";
 
 const Banner = () => {
+  const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="banner">
       <div className="banner__search">
-        <Button className="banner__searchButton" variant="outlined">
-          Search Dates
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchButton"
+          variant="outlined"
+        >
+          {showSearch ? "HIDE" : "Search Dates"}
         </Button>
       </div>
       <div className="banner__info">
-        <h1>ICHIRYU、新たな個性、個性を形に...Comming soon</h1>
+        <h1>新たな個性、個性を形に、無個性が個性を、Comming soon</h1>
         <h5>身長: 180 個性：一流</h5>
         <Button variant="outlined">私が来た</Button>
       </div>
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,46 @@
 import React from "react";
 import Banner from "./Banner";
+import Card from "./Card";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="home">
       <Banner />
+      <div className="home__section">
+        <Card
+          src="https://a0.muscache.com/im/pictures/ce6814ba-ed53-4d6e-b8f8-c0bbcf821011.jpg?im_w=720"
+          title="Drive"
+          description="Pic up me. Bro Bro Sorry Broooooo"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/ff69ac49-64e7-4f4a-ae2b-ee01163d0790.jpg?im_w=720"
+          title="親愛なる母"
+          description="ばあちゃんクッキング"
+        />
+        <Card
+          src="https://volleyball-assist.com/pic-labo/beachvolleyball640.jpg"
+          title="バレーボール"
+          description="バレーボール全日本女子"
+        />
+      </div>
+      <div className="home__section">
+        <Card
+          src="https://a0.muscache.com/im/pictures/ce6814ba-ed53-4d6e-b8f8-c0bbcf821011.jpg?im_w=720"
+          title="Drive"
+          description="Pic up me. Bro Bro Sorry Broooooo"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/a0316ecb-e49b-4b3a-b6b6-c2876b820e8c.jpg?im_w=720"
+          title="CAMP"
+          description="CAMPHACK"
+        />
+        <Card
+          src="https://volleyball-assist.com/pic-labo/beachvolleyball640.jpg"
+          title="バレーボール"
+          description="バレーボール全日本女子"
+        />
+      </div>
     </div>
   );
 }
